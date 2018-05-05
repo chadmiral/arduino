86ef4b6f8678212061a5796c0478147d441d8fe9
Chad Foxglove <chandra.foxglove@gmail.com>
2018-05-05 15:22:22 -0700

    janky fireplace lighting

--- fishing_shack_lights/fishing_shack_lights.ino
+++ fishing_shack_lights/fishing_shack_lights.ino
@@ -13,6 +13,7 @@ class Color
     float b;
 };
 
+Color COLOR_BLACK;
 Color Incandescent;
 
 int redPin = 11;
@@ -23,6 +24,8 @@ float tv_r = 0.0f;
 float tv_g = 0.0f;
 float tv_b = 0.0f;
 
+Color fireplace;
+
 void setup()
 {
   pinMode(redPin, OUTPUT);
@@ -32,18 +35,17 @@ void setup()
   Incandescent.r = 255.0f;
   Incandescent.g = 140.0f;
   Incandescent.b = 30.0f;
+
+  COLOR_BLACK.r = 0.0f;
+  COLOR_BLACK.g = 0.0f;
+  COLOR_BLACK.b = 0.0f;
 }
 
-void setColor(int red, int green, int blue)
+void setColor(Color c)
 {
-
-  red = 255 - red;
-  green = 255 - green;
-  blue = 255 - blue;
-
-  analogWrite(redPin, red);
-  analogWrite(greenPin, green);
-  analogWrite(bluePin, blue);
+  analogWrite(redPin, 255 - c.r);
+  analogWrite(greenPin, 255 - c.g);
+  analogWrite(bluePin, 255 - c.b);
 }
 
 float mix(float x, float y, float m)
@@ -63,11 +65,12 @@ void televisionLight()
   for(int i = 0; i < steps; i++)
   {
     float mix_val = (float)i / (float)steps;
-    float r = mix(tv_r, r_next, mix_val);
-    float g = mix(tv_g, g_next, mix_val);
-    float b = mix(tv_b, b_next, mix_val);
+    Color c;
+    c.r = mix(tv_r, r_next, mix_val);
+    c.g = mix(tv_g, g_next, mix_val);
+    c.b = mix(tv_b, b_next, mix_val);
 
-    setColor(r, g, b);
+    setColor(c);
     delay(10.0);
   }
 
@@ -79,17 +82,54 @@ void televisionLight()
 // as if someone keeps going in a room and turning on / off a light
 void periodicLight(Color c)
 {
-  setColor(c.r, c.g, c.b);
+  setColor(c);
   delay(random(2000.0f, 10000.0f));
-  setColor(0.0f, 0.0f, 0.0f);
+  setColor(COLOR_BLACK);
   delay(random(2000.0f, 10000.0f));
 }
 
-void flickerLight(float r, float g, float b)
+void fireplaceLight()
 {
-  setColor(r, g, b);
+  Color flame[3];
+  flame[0].r = 255.0f;
+  flame[0].g = 180.0f;
+  flame[0].b = 40.0f;
+
+  flame[1].r = 200.0f;
+  flame[1].g = 100.0f;
+  flame[1].b = 0.0f;
+
+  flame[2].r = 255.0f;
+  flame[2].g = 60.0f;
+  flame[2].b = 0.0f;
+
+  Color prev = fireplace;
+  Color next = flame[(int)random(0, 3)];
+
+  float brightness = 1.0f;//random(0.7f, 1.0f);
+
+  next.r = next.r * brightness;
+  next.g = next.g * brightness;
+  next.b = next.b * brightness;
+
+  int steps = random(20, 100);
+  for(int i = 0; i < steps; i++) {
+    Color c;
+    float mix_val = (float)i / (float)steps;
+    c.r = mix(prev.r, next.r, mix_val);
+    c.g = mix(prev.g, next.g, mix_val);
+    c.b = mix(prev.b, next.b, mix_val);
+    setColor(c);
+    delay(4.0f);
+  }
+  fireplace = next;
+}
+
+void flickerLight(Color c)
+{
+  setColor(c);
   delay(random(200.0f, 1200.0f));
-  setColor(0.0f, 0.0f, 0.0f);
+  setColor(COLOR_BLACK);
   delay(random(20.0f, 100.0f));
 }
 
@@ -97,5 +137,6 @@ void loop()
 {
   //televisionLight();
   //flickerLight(255.0f, 255.0f, 255.0f);
-  periodicLight(Incandescent);
+  //periodicLight(Incandescent);
+  fireplaceLight();
 }
